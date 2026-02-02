c16bfbd2cfa3cabff8f79c6dd8213c0dbd0a753b
guibaldissera <guilherme.baldissera@gmail.com>
2026-02-01 23:42:23 -0300

    feat: configure Stack navigation screens with titles and back button styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,16 @@ import { AlertProvider } from "../components";
 export default function RootLayout() {
   return (
     <AlertProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Payslips" }} />
+        <Stack.Screen
+          name="details"
+          options={{
+            title: "Payslip Details",
+            headerBackButtonDisplayMode: "minimal",
+          }}
+        />
+      </Stack>
     </AlertProvider>
   );
 }
